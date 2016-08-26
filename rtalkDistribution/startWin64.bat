e544ba9ac6da3d76c00c87f24115d3162c9d4f33
jtheberge <jtheberge@outlook.com>
2016-08-25 17:57:26 -0700

    Made guru bootable

--- rtalkDistribution/startWin64.bat
+++ rtalkDistribution/startWin64.bat
@@ -1,2 +1,2 @@
 @REM Rtalk Startup - Win64 version
-@java -agentlib:JvmtiDebug64 -cp RtalkOpen.jar;jars/* rtalk.RtalkMqttLauncher updateGuruChannel=updateGuru boot=bootRtalk.txt
+@java -cp RtalkOpen.jar;jars/* rtalk.RtalkMqttLauncher updateGuruChannel=updateGuru boot=bootRtalk.txt
