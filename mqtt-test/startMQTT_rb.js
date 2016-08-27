26dfff64c47841da8a72552e84ed33fc5c7c3b92
Ryan <ryan.benech@gmail.com>
2016-08-26 19:35:43 -0700

    minor

--- mqtt-test/startMQTT_rb.js
+++ mqtt-test/startMQTT_rb.js
@@ -5,6 +5,8 @@
   ------------------------------------------------------------------------------------------
 */
 var clientID = Math.random().toString(16).substr(2, 8);
+var cellID;
+
 console.log('Unique Client ID is '+ clientID);
 
 var mqtt    = require('mqtt');
@@ -13,20 +15,21 @@ var options = {
 	clientId: 'js' + clientID
 	};
 
-var client  = mqtt.connect('mqtt://localhost', options);
+var client  = mqtt.connect('mqtt://localhost:1883', options);
 
 client.on('connect', function () {
  // client.subscribe(clientID+'//cellinfo/#');
- client.subscribe('admin/#');
+ client.subscribe('+/+/whiteboard/#');
+ //client.subscribe('admin/#'); //hacky way
  // client.subscribe(clientID +'/X0M17536/whiteboard/createSubscriber/1',{qos:2});
- // client2.subscribe(clientID +'/whiteboard/createSubscriber/1',{qos:2});
+ // client.subscribe(clientID +'/whiteboard/createSubscriber/1',{qos:2});
  // client.subscribe('////createSubscriber/1');
  // client.subscribe('///whiteboard/createSubscriber/1');
   //client.subscribe('updateGuru/#');
  //client.publish('admin/cell/cellinfo/info/1',cbor.encode('iadminInfofcellIdHX0M17536'));
   //console.log('publish message:'+ cbor.encode('iadminInfofcellIdHX0M17536'));
  // '+/+/whiteboard/#', QOS 2
- // '+/+/whiteboard/createSubscriber/1' --> first token to subscribe /8/subscribe
+ // '%CliendID%/%CellID%/whiteboard/createSubscriber/1' --> first token to subscribe
  // 'reply address / cell id / channel / api / messageID'... 'view=config'
 //	'unique string made from me'
 //	'cellid = ' subscribe ''
@@ -38,11 +41,20 @@ client.on('error', function(err) {
 	console.log("Error: " + err.toString());
 });
 
-
 client.on('message', function (topic, message) {
   // message is Buffer
+  var flag = topic.toString();
+  flag = flag.substr(flag.length - 30);
+  if (flag=="/whiteboard/createSubscriber/1") {
+	cellID = topic.toString();
+	cellID = cellID.substr(9,8);
+	topic = clientID+"/"+cellID + '/whiteboard/createSubscriber/1';
+	client.subscribe(topic);
+	console.log('NOW Subscribed to '+ clientID+"/"+cellID + '/whiteboard/createSubscriber/1');
+	client.publish(topic,cbor.encode('@dviewJTranscript'),{qos:2});
+  }
   if (topic.toString()=="admin/cell/cellinfo/info/1") {
-	var cellID = message.toString();
+	cellID = message.toString();
 	cellID = cellID.substr(cellID.length - 8);
 	topic = clientID+"/"+cellID + '/whiteboard/createSubscriber/1';
 	client.subscribe(topic);
