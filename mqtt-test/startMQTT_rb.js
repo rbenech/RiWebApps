aab0da06bef1e9e836fd5c8beb61686a334736ff
Ryan <ryan.benech@gmail.com>
2016-08-26 17:21:33 -0700

    secret sauce

--- mqtt-test/startMQTT_rb.js
+++ mqtt-test/startMQTT_rb.js
@@ -6,7 +6,7 @@
 */
 
 var mqtt    = require('mqtt');
-var cbor		= require('cbor');
+var cbor	= require('cbor');
 var options = {
 	clientId: 'js' + Math.random().toString(16).substr(2, 8)
 	};
@@ -14,10 +14,18 @@ var options = {
 var client  = mqtt.connect('mqtt://localhost', options);
 
 client.on('connect', function () {
-  client.subscribe('admin/cell/#');
-  console.log('subscribe to admin/cell/#');
-  client.publish('admin/cell/cellinfo/info/1',cbor.encode('iadminInfofcellIdHX0M17536'));
-  console.log('publish message:'+ cbor.encode('iadminInfofcellIdHX0M17536'));
+  client.subscribe('///cellinfo/#');
+  client.subscribe('///whiteboard/#');
+  console.log('subscribe to +/+/whiteboard/#');
+ //client.publish('admin/cell/cellinfo/info/1',cbor.encode('iadminInfofcellIdHX0M17536'));
+  //console.log('publish message:'+ cbor.encode('iadminInfofcellIdHX0M17536'));
+ // '+/+/whiteboard/#', QOS 2
+ // '+/+/whiteboard/createSubscriber/1' --> first token to subscribe /8/subscribe
+ // 'reply address / cell id / channel / api / messageID'... 'view=config'
+//	'unique string made from me'
+//	'cellid = ' subscribe ''
+ // 'listed to messages that are objects'
+ // 
 });
 
 client.on('error', function(err) {
