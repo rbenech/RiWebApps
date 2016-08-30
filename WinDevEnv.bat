b34a1501c4e88040e19168baa046ab0bb896d5f8
jtheberge <jtheberge@roos.com>
2016-08-30 11:10:51 -0700

    Added bat file to automatically run moquette, startWin64, startMQTT_rb.js

---
+++ WinDevEnv.bat
@@ -0,0 +1,3 @@
+start /d "C:\Users\%username%\Dropbox\RiWebApps\rtalkDistribution\moquette\bin" moquette.bat
+start /d C:\Users\%username%\Dropbox\RiWebApps\rtalkDistribution\startWin64.bat
+watchify /d C:\Users\%username%\Dropbox\RiWebApps\mqtt-test\startMQTT_rb.js -o C:\Users\JTheberge\Dropbox\RiWebApps\mqtt-test\dist\bundle.js
